d82292f63d73adeaac0b126a599d4fe4ba8df3c3
localsagar <sagar@softpost.org>
2016-06-05 12:04:49 +1000

    first testng commit

--- src/main/java/org/softpost/App.java
+++ src/main/java/org/softpost/App.java
@@ -9,5 +9,15 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        print1();
+        print2();
+    }
+
+    private static void print1(){
+        System.out.println( "Print" );
+    }
+
+    private static void print2(){
+        System.out.println( "Print" );
     }
 }
